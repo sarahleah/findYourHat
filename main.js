d51936801a32e084eefb50e3d96660b44bc6ea60
sarahandreas <sarah.andreas@gmail.com>
2021-08-11 18:02:15 +1000

    made loop, shows movement

--- main.js
+++ main.js
@@ -11,8 +11,9 @@ class Field {
   }
 
   print() {
-      for (let i in this.fieldArr) {
-          this.fieldArr[i] = this.fieldArr[i].join('');
+      for (let item of this.fieldArr) {
+          console.log(item);
+          item = item.join('');
       }
       console.log(this.fieldArr.join('\n'));
   }
@@ -27,33 +28,52 @@ const myField = new Field([
 // myField.print();
 
 //get user current position
-  //position = [y[x]]
+  //position = [y][x]
 let y = 0;
 let x = 0;
-let position = myField.fieldArr[y[x]]
 
-//get user input
-let move = prompt('Which way?\nw for up\ns for down\nd for right\na for left');
-switch (move) {
-    case 'w':
-        //w up
+let position = myField.fieldArr[y][x]
+
+myField.print()
+
+while (true) {
+
+    // checks whether pos is defined
+    try {
+        position = myField.fieldArr[y][x]
+    } catch(err) {
+        console.log("You're out of bounds")
+        return null
+    }
+
+
+    // checks if user is on hat/inhole
+    if (position === 'O') {
+        console.log('You fell in a hole!')
+        return null
+    } else if (position === '^') {
+        console.log('You found your hat!')
+        return null
+    } else {
+        myField.fieldArr[y][x] = '*'
+    }
+        
+
+    // get user input
+    let move = prompt('Which way? w for up, s for down, d for right, a for left');
+
+    if (move === 'w') {
         y -= 1;
-        break;
-    case 's': 
-        //s down
+    } else if (move === 's') {
         y += 1;
-        break;
-    case 'd': 
-        //d right
+    } else if (move === 'd') {
         x += 1;
-        break;
-    case 'a':
-        //a left
+    } else if (move === 'a') {
         x -= 1;
-        break;
-}
-//print current field
-position = myField.fieldArr[y[x]];
-//check whether input is out of bounds/on hat/in hole
-//tell user
+    } else {
+        console.log('Try again')
+    }
 
+    //prints out field for user
+    myField.print()
+}
